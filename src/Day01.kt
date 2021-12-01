4ab00cb6e35b735695b0598f0843815893daa389
szy-puszcz <szymon.puszcz@rsqtechnologies.com>
2021-12-01 19:55:09 +0100

    day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,32 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+    fun countIncreases(input: List<Int>) =
+        input.zipWithNext()
+            .filter { it.first < it.second }
+            .count()
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    fun part1(input: List<Int>): Int =
+        countIncreases(input)
 
-    val input = readInput("Day01")
+    fun part2(input: List<Int>): Int =
+        input.zipInTriples()
+            .map { it.sum() }
+            .let {
+                countIncreases(it)
+            }
+
+    val input = readInputAsInt("Day01")
+
     println(part1(input))
+
     println(part2(input))
 }
+
+private fun Triple<Int, Int, Int>.sum(): Int =
+    first + second + third
+
+private fun <T> List<T>.zipInTriples() =
+    zip(drop(1))
+        .zip(drop(2)) { (a, b), c ->
+            Triple(a, b, c)
+        }
--- src/Utils.kt
+++ src/Utils.kt
@@ -5,9 +5,16 @@ import java.security.MessageDigest
 /**
  * Reads lines from the given input txt file.
  */
-fun readInput(name: String) = File("src", "$name.txt").readLines()
+fun readInput(name: String) =
+    File("src", "$name.txt").readLines()
+
+fun readInputAsInt(name: String) =
+    File("src", "$name.txt")
+        .readLines()
+        .map { it.toInt() }
 
 /**
  * Converts string to md5 hash.
  */
-fun String.md5(): String = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray())).toString(16)
+fun String.md5(): String =
+    BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray())).toString(16)
